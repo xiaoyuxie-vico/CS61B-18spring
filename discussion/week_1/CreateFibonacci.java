e38a7b68f6ffc2b56177c4295649c5547f45b410
xiaoyu.xie <xiaoyuxie.vico@gmail.com>
2020-05-14 17:08:14 +0800

    discussion public class CreateFibonacci {

---
+++ discussion/week_1/CreateFibonacci.java
@@ -0,0 +1,17 @@
+/** A client that uses the synthesizer package to replicate a plucked guitar string sound */
+public class CreateFibonacci {
+    public static int fib(int n) {
+        if ((n == 1) || (n == 2)) {
+            return 1;
+        } else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int fib_num;
+        fib_num = fib(8);
+        System.out.println(fib_num);
+    }
+}
+
