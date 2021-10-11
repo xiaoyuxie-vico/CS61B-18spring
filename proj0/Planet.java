0ef26cac7fe60929c9123f8d01e67c6eed776158
xiaoyu.xie <xiaoyuxie.vico@gmail.com>
2021-10-11 10:46:04 -0500

    fix public variable

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,12 +1,12 @@
 import java.lang.Math;
 
 public class Planet {
-    double xxPos; //Its current x position
-    double yyPos; //Its current y position
-    double xxVel; //Its current velocity in the x direction
-    double yyVel; //Its current velocity in the y direction
-    double mass;  //Its mass
-    String imgFileName;
+    public double xxPos; //Its current x position
+    public double yyPos; //Its current y position
+    public double xxVel; //Its current velocity in the x direction
+    public double yyVel; //Its current velocity in the y direction
+    public double mass;  //Its mass
+    public String imgFileName;
 
     /*
     First constructor
